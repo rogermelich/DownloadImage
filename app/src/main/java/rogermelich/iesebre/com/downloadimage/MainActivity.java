51a28ea86645a1efc76376ff30644656fd9cfe36
rogermelich <rogermelich@gmail.com>
2015-12-18 14:30:25 +0100

    Modified from example wiki

--- app/src/main/java/rogermelich/iesebre/com/downloadimage/MainActivity.java
+++ app/src/main/java/rogermelich/iesebre/com/downloadimage/MainActivity.java
@@ -1,15 +1,23 @@
 package rogermelich.iesebre.com.downloadimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ImageView = (ImageView) findViewById(R.id.imatgeDescarregada);
+
+
+                    }
+
+                private Bitmap loadImageFromNetwork(String url){
+                try {
+                        Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+                        return bitmap;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                return null;
+            }
+
+                public void onClick(View v) {
+                new Thread(new Runnable() {
+                        public void run() {
+                                Bitmap b = loadImageFromNetwork("http://www.comolohago.cl/wp-content/uploads/2013/06/ANDROID.png");
+                                ImageView.setImageBitmap(b);
+                            }
+                    }).start();
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
